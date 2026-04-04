880e0e3212489f37933b363c021ec469bbdc2c85
Wasiul Islam Sakif <sakif049@gmail.com>
2026-04-04 17:35:31 +0600

    fix: expire date

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  DateTime expiredDate = DateTime.parse("2026-04-05 10:20:57.556371");
+  DateTime expiredDate = DateTime(2026, 4, 8);
 
   RxBool isExpired = false.obs;
 
